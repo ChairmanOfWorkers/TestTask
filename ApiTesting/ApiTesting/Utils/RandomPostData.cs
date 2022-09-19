4ae05c619f76501bcb6e343c680599f9f7488f74
Farrukh <farrukhgayipov@gmail.com>
2022-09-19 11:30:24 +0500

    initial commit

---
+++ ApiTesting/ApiTesting/Utils/RandomPostData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ApiTesting.Utils
+{
+    public static class RandomPostData
+    {
+        private static Random random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ ApiTesting/ApiTesting/Utils/TestDataUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using ApiTesting.Models;
+
+namespace ApiTesting.Utils
+{
+    public static class TestDataUtils
+    {
+        private static readonly string path = $"{AppDomain.CurrentDomain.BaseDirectory}\\Resource\\testData.json";
+        public static UserData TestDataFile()
+        {
+            return JsonConvert.DeserializeObject<UserData>(File.ReadAllText(path));
+        }
+    }
+}
